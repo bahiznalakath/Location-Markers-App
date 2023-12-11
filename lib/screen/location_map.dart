755b5f9765b15b7ea0168f0d7df38e5e8822563c
dishankunnummal <bahiznalakath123@gmail.com>
2023-12-11 17:27:27 +0530

    Showing current location on Google Map

--- lib/screen/location_map.dart
+++ lib/screen/location_map.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
-
 import '../Provider/databaseController.dart';
 import '../Provider/locationController.dart';
 import '../model/model_class.dart';
@@ -15,8 +14,6 @@ class LocationMap extends StatefulWidget {
 }
 
 class _LocationMapState extends State<LocationMap> {
-
-
   Completer<GoogleMapController> _controller = Completer();
 
   static const CameraPosition _kGoogle = CameraPosition(
@@ -26,26 +23,54 @@ class _LocationMapState extends State<LocationMap> {
 
   @override
   Widget build(BuildContext context) {
-    var prov =
-    Provider.of<LocationProvider>(context, listen: false);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Google Maps"),
-        ),
-        body: ValueListenableBuilder<List<LocationModel>>(
-          valueListenable: locationListNotifier,
-          builder: (BuildContext context, List<LocationModel> locationList,
-              Widget? child) {
-            return GoogleMap(
-              initialCameraPosition: _kGoogle,
-              // markers: {
-              //   Marker(markerId: MarkerId("soucre"),position: _kGoogle)
-              // },
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            );
-          },
-        ));
+      appBar: AppBar(
+        title: const Text("Google Maps"),
+      ),
+      body: ValueListenableBuilder<List<LocationModel>>(
+        valueListenable: locationListNotifier,
+        builder: (BuildContext context, List<LocationModel> locationList, Widget? child) {
+          return GoogleMap(
+            initialCameraPosition: _kGoogle,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            markers: _buildMarkers(locationList, context),
+          );
+        },
+      ),
+    );
   }
+
+  Set<Marker> _buildMarkers(List<LocationModel> locationList, BuildContext context) {
+    Set<Marker> markers = Set();
+
+    // Add a green marker for the current location
+    final currentLocationProvider = Provider.of<LocationProvider>(context);
+    double currentLatitude = double.tryParse(currentLocationProvider.Latitude) ?? 0.0;
+    double currentLongitude = double.tryParse(currentLocationProvider.Longitude) ?? 0.0;
+
+    markers.add(Marker(
+      markerId: MarkerId("currentLocation"),
+      position: LatLng(currentLatitude, currentLongitude),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+      infoWindow: InfoWindow(title: "Current Location"),
+    ));
+
+    // Add red markers for saved locations
+    for (LocationModel location in locationList) {
+      double savedLatitude = double.tryParse(location.latitude) ?? 0.0;
+      double savedLongitude = double.tryParse(location.longitude) ?? 0.0;
+
+      markers.add(Marker(
+        markerId: MarkerId(location.id.toString()),
+        position: LatLng(savedLatitude, savedLongitude),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        infoWindow: InfoWindow(title: "Saved Location"),
+      ));
+    }
+
+    return markers;
+  }
+
 }
